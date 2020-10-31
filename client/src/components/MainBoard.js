d7796670d1f85e1c03d578fd7eeedf930d60b5d0
kumenger <44558777+kumenger@users.noreply.github.com>
2020-10-31 19:29:06 +0300

    six

--- client/src/components/MainBoard.js
+++ client/src/components/MainBoard.js
@@ -9,23 +9,21 @@ import {
 import { getIdForReplay } from "../actions";
 import { Accordion, Figure } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import Signup from './Signup'
+import Signup from "./Signup";
 import FigureCaption from "react-bootstrap/esm/FigureCaption";
 import axios from "axios";
-import CommentBox from './CommentBox'
-
+import CommentBox from "./CommentBox";
 
 const MainBoard = (props) => {
   let myarry = [
     "f7af89",
     "e6e6fa",
     "e9a6af",
-    "ff634d",
+    
     "21b1ff",
-    
-    "9662eb",
+
     "ebffc0",
-    
+
     "aaccee",
     "e9bfa6",
     "6aa6e1",
@@ -36,18 +34,14 @@ const MainBoard = (props) => {
   ];
 
   const [data, setdata] = useState([]);
-   const[len,setl]=useState(0)
+  const [len, setl] = useState(0);
 
   useEffect(() => {
-   
-      
-    
-    props.loadAllPost()
+    props.loadAllPost();
     props.getIdForReplay(null);
-    
+
     props.checkPageCreateOrReplay(true);
   }, []);
-  
 
   if (!props.allposts) {
     return (
@@ -58,7 +52,6 @@ const MainBoard = (props) => {
   }
 
   return (
-    
     <div
       className="row rounded-right rounded-left rounded-bottom rounded-top overflow-auto"
       style={{
@@ -66,25 +59,26 @@ const MainBoard = (props) => {
         paddingLeft: "30px",
       }}
     >
-   
-
-      <div className="col-md-8 scrollbar "  style={{overflowY:"scroll",height:"80vh",padding:"20px"}}>
+      <div
+        className="col-md-8 scrollbar "
+        style={{ overflowY: "scroll", height: "80vh", padding: "20px" }}
+      >
         {props.allposts.map((x, index, arr) => {
           return (
             <div key={index} style={{}}>
               <div
                 className="row rounded-right rounded-left rounded-bottom rounded-top  "
                 style={{
-                 backgroundColor: "snow",
-                
+                  backgroundColor: "snow",
+
                   paddingTop: "5px",
                   color: "BLACK",
-                  border:"1px solid black"
+                  border: "1px solid black",
                 }}
               >
                 <div className="col-md-6 ">
                   <Link
-                  className='googlefont'
+                    className="googlefont"
                     to={`PostReplay/${x._id}`}
                     onClick={() => props.getIdForReplay(x._id)}
                   >
@@ -98,25 +92,24 @@ const MainBoard = (props) => {
                   >{`${x.time}`}</p>
                 </div>
 
-                <div className="col-md-2" >
-                
-                    <img   alt={`${x.replay&&x.replay.length>0?x.replay.length:0}`}
-                    
-                    style={{maxWidth:"100%"}}
-                      className="rounded-right rounded-left rounded-bottom rounded-top border"
-                      src={
-                        props.facebookloginreducer.isLogIn &&
-                        props.facebookloginreducer.resp.userID === x.userID &&
-                        !props.facebookloginreducer.resp.error
-                          ? props.facebookloginreducer.resp.picture.data.url
-                          : `https://ui-avatars.com/api/?name=${
-                             x.replay&&x.replay.length > 0 &&
-                              x.replay[x.replay.length - 1].hasOwnProperty(
-                                "replayername"
-                              )
-                                ? x.replay[x.replay.length - 1].replayername
-                                : x.name
-                            }&size=64&font-size=0.2
+                <div className="col-md-2">
+                  <img
+                    style={{ maxWidth: "100%" }}
+                    className="rounded-right rounded-left rounded-bottom rounded-top border"
+                    src={
+                      props.facebookloginreducer.isLogIn &&
+                      props.facebookloginreducer.resp.userID === x.userID &&
+                      !props.facebookloginreducer.resp.error
+                        ? props.facebookloginreducer.resp.picture.data.url
+                        : `https://ui-avatars.com/api/?name=${
+                            x.replay &&
+                            x.replay.length > 0 &&
+                            x.replay[x.replay.length - 1].hasOwnProperty(
+                              "replayername"
+                            )
+                              ? x.replay[x.replay.length - 1].replayername
+                              : x.name
+                          }&size=64&font-size=0.2
                         
                           &background=${
                             myarry[Math.round(Math.random() * myarry.length)]
@@ -124,15 +117,36 @@ const MainBoard = (props) => {
                 
               
           `
-                      }
-                    />
-                   <p>{x.replay&&x.replay.length>0?<span class="fas fa-comment " style={{color:"blueviolet"}}>{x.replay.length}</span>:""}        {x.likes&&x.likes>0?<span class="fas fa-heart" style={{color:"palevioletred"}}>{x.likes}</span>:""} </p>   
-                  
+                    }
+                  />
+                  <p style={{fontSize:"14px"}} className='text-left'>
+                    {x.replay && x.replay.length > 0 ? (
+                      <span
+                        class="fas fa-comment "
+                        style={{ color: "blueviolet" }}
+                      >
+                        {x.replay.length}
+                      </span>
+                    ) : (
+                      ""
+                    )}{" "}
+                    {x.likes && x.likes > 0 ? (
+                      <span
+                        class="fas fa-heart"
+                        style={{ color: "palevioletred" }}
+                      >
+                        {x.likes}
+                      </span>
+                    ) : (
+                      ""
+                    )}{" "}
+                  </p>
                 </div>
                 <div className="col-md-4">
                   <p style={{ fontSize: "14px" }}>
                     last reply by{" "}
-                    {x.replay&&x.replay.length > 0 &&
+                    {x.replay &&
+                    x.replay.length > 0 &&
                     x.replay[x.replay.length - 1].hasOwnProperty("replayername")
                       ? x.replay[x.replay.length - 1].replayername
                       : x.name}
@@ -148,36 +162,76 @@ const MainBoard = (props) => {
               </div>
 
               <br></br>
-              
             </div>
-            
           );
         })}
       </div>
-      <div className='col-md-4 '  >
-   
-         
-        <div className='row'>
-        <div className='col-md-12'>
-             <div className='row'>
-             <div className='col-md-12 text-center text-primary '>
-<Link 
-               onClick={() => props.getIdForReplay(props.allposts[props.allposts.length-1]._id)}
-             to={`/PostReplay/${props.allposts[props.allposts.length-1]._id}`} 
-             style={{fontSize:"20px"}}  className='text-center text-primary'>{props.allposts[props.allposts.length-1].title}</Link></div>
-             <div className=' col-md-12'><p className='text-left'>{props.allposts[props.allposts.length-1].post}</p></div>
-             </div>
+      <div className="col-md-4 " style={{paddingTop:"2%"}}>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="row">
+              <div className="col-md-12 text-center text-primary ">
+                <Link
+                  onClick={() =>
+                    props.getIdForReplay(
+                      props.allposts[props.allposts.length - 1]._id
+                    )
+                  }
+                  to={`/PostReplay/${
+                    props.allposts[props.allposts.length - 1]._id
+                  }`}
+                  style={{ fontSize: "20px" }}
+                  className="text-center text-primary"
+                >
+                  {props.allposts[props.allposts.length - 1].title}
+                </Link>
+              </div>
 
-
+              <div className=" col-md-12">
+                <div className="row">
+                  <div className="col-md-3">
+                    <img
+                      style={{ maxWidth: "100%" }}
+                      className="rounded-right rounded-left rounded-bottom rounded-top "
+                      src={
+                        props.facebookloginreducer.isLogIn &&
+                        props.facebookloginreducer.resp.userID ===
+                          props.allposts[props.allposts.length - 1].userID &&
+                        !props.facebookloginreducer.resp.error
+                          ? props.facebookloginreducer.resp.picture.data.url
+                          : `https://ui-avatars.com/api/?name=${
+                              props.allposts[props.allposts.length - 1].name
+                            }&size=128&font-size=0.2&rounded=true
+                        
+                          &background=${
+                            myarry[Math.round(Math.random() * myarry.length)]
+                          }
+                
+              
+          `
+                      }
+                    />
+                  </div>
+                  <div className="offfset-md-1 col-md-8" style={{height:"50vh",overflow:"hidden"}}>
+                    <p className="text-left" >
+                      {props.allposts[props.allposts.length - 1].post}.....
+                    </p>
+                  </div>
+                  <br></br>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className='col-md-12'> <h6 className='text-right text-info'>Post by {   props.allposts[props.allposts.length-1].name} on {props.allposts[props.allposts.length-1].time}</h6></div>
-
-       
          
+          <div className="col-md-12">
+          <br></br>
+            <h6 className=" text-info text-center">
+            Recent Post by {props.allposts[props.allposts.length - 1].name} on{" "}
+              {props.allposts[props.allposts.length - 1].time}
+            </h6>
+          </div>
         </div>
-      
       </div>
-      
     </div>
   );
 };
@@ -188,7 +242,7 @@ const mapStateToProps = (state) => {
     getIdForReplayReducer: state.getIdForReplayReducer,
     getInsideIndexforlikesreducer: state.getInsideIndexforlikesreducer,
     checkpagetypereducer: state.checkpagetypereducer,
-    mongologinreduxer:state.mongologinreduxer,
+    mongologinreduxer: state.mongologinreduxer,
   };
 };
 
