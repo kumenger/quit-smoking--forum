783d885cf477af6643b6f208e05e1af32d3cec24
kumenger <44558777+kumenger@users.noreply.github.com>
2023-05-11 00:54:27 -0600

    style

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,15 +3,15 @@ import React from "react";
 const home = () => {
     return (
      
-        <div style={{ paddingTop:"30px"}}
+        <div style={{ padding:"30px"}}
           className="row "
           
         >
           
          
-          <div className="  col-xs-12 col-s-12 col-md-5 col-l-6   text-center "  >
+          <div className="  col-xs-12 col-s-12 col-md-5 col-l-6    text-right "  >
        
-          <img className="img-fluid img-thumbnail " width="75%" 
+          <img className="img-fluid img-thumbnail " width="100%" 
           src='https://i.ibb.co/VHk07ng/img-l-HSBc-HSy-YQFW3r-Ribh-B8-We-UE.png'
          // src='https://i.ibb.co/RvRdq8m/img-utp-OTTjdn-EAT5-Rl-Fl3-F19r-Ix.png'
           />
@@ -19,7 +19,7 @@ const home = () => {
           </div>
           
           <div className=" col-xs-12 col-s-12
-             col-md-5 col-l-6   "  style={{letterSpacing:"1px",padding:"30px"}} >
+             col-md-6 col-l-6   "  style={{letterSpacing:"1px"}} >
              
           
 
@@ -35,11 +35,11 @@ const home = () => {
                     }>
                         In as little as <span style={{color:"#DC143C"}} >20 minutes</span>  after the last cigarette is smoked, the
                                                               heart rate drops and returns to normal
-                        <img src="https://img.icons8.com/bubbles/30/null/happy.png"/>. Blood pressure begins to
+                                                              <img src="https://img.icons8.com/arcade/20/null/exclamation-mark.png"/> Blood pressure begins to
                                                               drop, and circulation may start to improve.
 
                     </p>
-                   
+                   <hr></hr>
                     <p 
                         style={
                             {
@@ -49,12 +49,13 @@ const home = () => {
                                 fontWeight: ""
                             }
                     }>
-                        After just<span style={{color:"#DC143C"}}>12 hours </span>   without a cigarette, the body cleanses itself of the excess carbon
-                                                              monoxide from the cigarettes.<img src="https://img.icons8.com/bubbles/30/null/happy.png"/>
+                        After just <span style={{color:"#DC143C"}}>12 hours </span>   without a cigarette, the body cleanses itself of the excess carbon
+                                                              monoxide from the cigarettes
+                                                              <img src="https://img.icons8.com/arcade/20/null/exclamation-mark.png"/>
                         The carbon monoxide level returns to
                                                               normal, increasing the body’s oxygen levels
                     </p>
-                   
+                   <hr></hr>
                     <p 
                         style={
                             {
@@ -66,11 +67,11 @@ const home = () => {
                     }>
                         Just <span style={{color:"#DC143C"}} >1 day</span> after quitting smoking, the risk of heart attack begins
                                                               to decrease
-                        <img src="https://img.icons8.com/bubbles/30/null/happy.png"/>. Smoking raises the risk of developing coronary heart
+                                                              <img src="https://img.icons8.com/arcade/20/null/exclamation-mark.png"/> Smoking raises the risk of developing coronary heart
                                                               disease by lowering good cholesterol, which makes heart-healthy
                                                               exercise harder to do.
                     </p>
-                   
+                   <hr></hr>
                     <p 
                         style={
                             {
@@ -83,11 +84,11 @@ const home = () => {
                         Smoking damages the nerve endings responsible for the senses of
                                                               smell and taste. <br></br>In as little as <span style={{color:"#DC143C"}} >2 days </span>after quitting, a person
                                                               may notice a heightened sense of smell
-                        <img src="https://img.icons8.com/bubbles/30/null/happy.png"/>
+                                                           
                         and more vivid tastes as
-                                                              these nerves heal.
+                                                              these nerves heal   <img src="https://img.icons8.com/arcade/20/null/exclamation-mark.png"/>
                     </p>
-                    <h2 style={
+                    <hr></hr>  <h2 style={
                         {
                             color: "#DC143C",
                            
@@ -95,8 +96,10 @@ const home = () => {
                             fontWeight:"bold"
                           
                         }
-                    }>Quitting  Never<br></br>
+                    }>Quitting Has Never
                     Felt So Good <img src="https://img.icons8.com/arcade/30/null/exclamation-mark.png"/></h2>
+                    <a style={{textDecoration:"underline",alignSelf:"center"}} href="https://oswaldspharmacy.com/2023/05/04/quitting-has-never-felt-so-good-smoking-cessation/" target="_blank">More Help</a>
+                  
             
           </div>
           
